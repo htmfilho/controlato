7d4ff989edb781c37f8f47695ca863dccafdfb09
Hildeberto Mendonca <me@hildeberto.com>
2020-03-22 00:13:02 -0400

    Added the formula field to the indicator table.

--- monitoring/models/indicator.py
+++ monitoring/models/indicator.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 
 
 class IndicatorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'scale',)
-    fieldsets = ((None, {'fields': ('name', 'scale',)}),)
+    list_display = ('name', 'scale', 'formula',)
+    fieldsets = ((None, {'fields': ('name', 'scale', 'formula',)}),)
 
 
 class Indicator(models.Model):
     scale = models.ForeignKey('Scale', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    formula = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
