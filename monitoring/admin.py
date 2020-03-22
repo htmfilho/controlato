b54ed92f8a068e044a197d441013da1495d7a593
Hildeberto Mendonca <me@hildeberto.com>
2020-03-21 21:15:47 -0400

    Added the indicator composition

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from monitoring.models import indicator, indicator_context, scale, unit, unit_conversion
+from monitoring.models import indicator, indicator_composition, indicator_context, scale, unit, unit_conversion
 
 
 admin.site.register(indicator.Indicator, indicator.IndicatorAdmin)
+admin.site.register(indicator_composition.IndicatorComposition, indicator_composition.IndicatorCompositionAdmin)
 admin.site.register(indicator_context.IndicatorContext, indicator_context.IndicatorContextAdmin)
 admin.site.register(scale.Scale, scale.ScaleAdmin)
 admin.site.register(unit.Unit, unit.UnitAdmin)
