ba02cb85522e56367585f7051b94e710941e546e
Hildeberto Mendonca <me@hildeberto.com>
2020-03-22 00:13:53 -0400

    Added measurement model

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from monitoring.models import indicator, indicator_composed, indicator_composition, indicator_context, scale, unit, \
-    unit_conversion
+from monitoring.models import indicator, indicator_composed, indicator_composition, indicator_context, measurement, \
+    scale, unit, unit_conversion
 
 
 admin.site.register(indicator.Indicator, indicator.IndicatorAdmin)
 admin.site.register(indicator_composed.IndicatorComposed, indicator_composed.IndicatorComposedAdmin)
 admin.site.register(indicator_composition.IndicatorComposition, indicator_composition.IndicatorCompositionAdmin)
 admin.site.register(indicator_context.IndicatorContext, indicator_context.IndicatorContextAdmin)
+admin.site.register(measurement.Measurement, measurement.MeasurementAdmin)
 admin.site.register(scale.Scale, scale.ScaleAdmin)
 admin.site.register(unit.Unit, unit.UnitAdmin)
 admin.site.register(unit_conversion.UnitConversion, unit_conversion.UnitConversionAdmin)
