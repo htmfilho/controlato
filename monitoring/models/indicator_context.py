119e77a34feb3103ecd3b2d46f830517cc6136b9
Hildeberto Mendonca <me@hildeberto.com>
2020-03-21 21:06:03 -0400

    Linked the Indicator Context to the Indicator

--- monitoring/models/indicator_context.py
+++ monitoring/models/indicator_context.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 
 
 class IndicatorContextAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    fieldsets = ((None, {'fields': ('name',)}),)
+    list_display = ('name', 'indicator',)
+    fieldsets = ((None, {'fields': ('name', 'indicator',)}),)
 
 
 class IndicatorContext(models.Model):
+    indicator = models.ForeignKey('Indicator', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
     def __str__(self):
