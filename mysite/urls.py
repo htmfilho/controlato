5e36c8486061b4d73d474e83e8f8eb964baf7b8a
Hildeberto Mendonca <me@hildeberto.com>
2020-03-22 10:06:29 -0400

    Introduced the HTML template and the index page.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('monitoring.urls')),
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = 'Controlato Administration'
+admin.site.site_title = 'Administration'
+admin.site.index_title = 'Controlato'
