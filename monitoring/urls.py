261384b5262ecca5a171e54c43e6e4b09348ca91
Kenia Sousa <kenia.sousa@gmail.com>
2020-12-04 23:03:10 -0500

    Removed the Django app

--- monitoring/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('contexts/<int:context_id>/', views.context, name='context'),
-    path('contexts/<int:context_id>/chart', views.context_chart),
-]
